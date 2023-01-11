0320b976fcc2255d117188bb12085aa615636f97
André <AndreR@ppedv.de>
2023-01-11 13:17:42 +0100

    Dekorator patterrn

---
+++ HalloDekorator/HalloDekorator/Deko.cs
@@ -0,0 +1,56 @@
+﻿namespace HalloDekorator
+{
+    internal interface IComponent
+    {
+        string Beschreibung { get; }
+        decimal Preis { get; }
+    }
+
+    class Pizza : IComponent
+    {
+        public string Beschreibung => "Pizza";
+
+        public decimal Preis => 7m;
+    }
+
+    class Brot : IComponent
+    {
+        public string Beschreibung => "Brot";
+
+        public decimal Preis => 4m;
+    }
+
+    abstract class Deko : IComponent
+    {
+        protected readonly IComponent parent;
+
+        public Deko(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Beschreibung { get; }
+
+        public abstract decimal Preis { get; }
+    }
+
+    class Salami : Deko
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Salami";
+
+        public override decimal Preis => parent.Preis + 2.3m;
+    }
+
+    class Käse : Deko
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Käse";
+
+        public override decimal Preis => parent.Preis + 1.5m;
+    }
+}
---
+++ HalloDekorator/HalloDekorator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDekorator;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+
+var pizza = new Käse(new Salami(new Käse(new Pizza())));
+Console.WriteLine($"{pizza.Beschreibung} {pizza.Preis:c}");
