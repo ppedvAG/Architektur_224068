b06274362fe709e0f5145957c2824f9f6be19645
André <AndreR@ppedv.de>
2023-01-11 10:24:40 +0100

    Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace HalloSingelton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private static object _sync = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_sync)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Log("Neuer Logger");
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:F}] {message}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+//var logger = new Logger();
+//logger.Log("Hallo");
+
+for (int i = 0; i < 10; i++)
+{
+    Task.Run(() => Logger.Instance.Log($"Hallo {i}"));
+}
+
+
+Logger.Instance.Log("Hallo 1");
+Logger.Instance.Log("Hallo 2");
+Logger.Instance.Log("Hallo 3");
