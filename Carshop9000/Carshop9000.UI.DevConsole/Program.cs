d4e1a5e51c8c680e54b09053f75815644cdd3476
André <AndreR@ppedv.de>
2023-01-12 11:43:44 +0100

    Bestes UI

---
+++ Carshop9000/Carshop9000.UI.DevConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using Carshop9000.Data.EfCore;
+using Carshop9000.Logic;
+using Carshop9000.Model.DomainModel;
+
+Console.WriteLine("*** Carshop 9000 v0.1 ***");
+
+var conString = "Server=(localdb)\\mssqllocaldb;Database=Carshop9000_Test;Trusted_Connection=true";
+var repo = new EfRepository(conString);
+
+var carService = new CarService(repo);
+
+carService.CreateDemoCars();
+
+foreach (var car in repo.GetAll<Car>())
+{
+    Console.WriteLine($"{car.Model} {car.Color}");
+}
